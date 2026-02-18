503d681c99e15a4b1c1a750c93b23da42eefbc6d
Tristan Caluag <97552369+tkcaluag@users.noreply.github.com>
2026-02-18 10:19:43 -0800

    Finished Build
    Coins, Win / Game Over Screens, Obstacles

--- Assets/Scenes/Scripts/PlayQuit Script.cs
+++ Assets/Scenes/Scripts/PlayQuit Script.cs
@@ -6,6 +6,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayGame()
     {
+        Time.timeScale = 1;
         UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
     }
 
